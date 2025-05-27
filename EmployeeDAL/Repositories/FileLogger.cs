3af7c793faa9d9f5c3b3770b7700e0c5da436627
Gautam-Lathiya <gautamlathiya18@gmail.com>
2025-05-27 17:45:23 +0530

    Add more C# 13 features : Partial property

--- EmployeeDAL/Repositories/FileLogger.cs
+++ EmployeeDAL/Repositories/FileLogger.cs
@@ -6,19 +6,23 @@ using System.Threading.Tasks;
 
 namespace EmployeeDAL.Repositories
 {
-    public sealed class FileLogger
+    public sealed partial class FileLogger
     {
         private static readonly FileLogger _instance = new FileLogger();
         private static readonly string logFilePath = @"../EmployeeDAL/log.txt";
         private static readonly Lock _lock = new();    // C#13 feature: Lock type
 
+        // Partial property declaration
+        public partial string LogHeader { get; set; }
+
         public static FileLogger Instance()
         {
             return _instance;
         }
+
         public void Log(params List<string> messages)  // C#13 feature: params collections
         {
-            File.AppendAllText(logFilePath, $"{DateTime.Now}: ");
+            File.AppendAllText(logFilePath, $"{DateTime.Now}: {LogHeader}");
             messages.ForEach(LogMessage); // C#13 feature: Method group natural type
         }
 
@@ -28,6 +32,18 @@ namespace EmployeeDAL.Repositories
             {
                 File.AppendAllText(logFilePath, $"{message}{Environment.NewLine}");
             }
+        }
+    }
+
+    // Another part of the partial class defining the property implementation
+    public sealed partial class FileLogger
+    {
+        private string _logHeader = "System Log: ";
+
+        public partial string LogHeader
+        {
+            get => _logHeader;
+            set => _logHeader = value;
         }
     }
 }
